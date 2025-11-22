83cab51d63fb79e092b80f5aa89e643e66ebed72
My Name <nithin.m.gowda2606@gmail.com>
2025-11-22 16:29:16 +0100

    line

--- render_first_map.py
+++ render_first_map.py
@@ -1,5 +1,28 @@
 import folium
+from folium import plugins
 
-mymap = folium.Map(location = [52.5036504, 13.4447152], zoom_start = 14)
+berlin_center = [52.5036504, 13.4447152]
+mymap = folium.Map(location = berlin_center, zoom_start = 14)
+
+
+point1 = [52.5163, 13.4050]
+point1_name = "Brandenburg Gate"
+
+point2 = [52.5219, 13.4132]
+point2_name = "Alexa"
+
+folium.Marker(location = point1,
+              popup = point1_name
+              ).add_to(mymap)
+
+folium.Marker(location = point2,
+              popup = point2_name
+              ).add_to(mymap)
+
+folium.PolyLine(
+    locations=[point1,point2]
+).add_to(mymap)
+
+# plugins.AntPath(locations=[point1, point2]).add_to(mymap)
 
 mymap.show_in_browser()
